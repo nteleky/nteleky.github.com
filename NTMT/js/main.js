a46c10c6e02b19db1c8c130646ad6ba0a48f6e31
Nick Teleky <nteleky@gmail.com>
2013-04-23 16:47:43 -0400

    Changed color scheme to b/w and cleaned html

--- NTMT/js/main.js
+++ NTMT/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $(".load").hide();
-    $("#h01").html("Hello jQuery").delay(1000);
-    $(".loadone").delay(1800).fadeIn("slow");
+    $(".loadone").delay(500).fadeIn("slow");
 
 });
