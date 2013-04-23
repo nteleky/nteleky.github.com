10e36a35e5e4f11d8ceac37f073f420609612aea
Nick Teleky <nteleky@gmail.com>
2013-04-23 19:31:59 -0400

    changed color scheme and added animation and logo to homepage.

--- NTMT/js/main.js
+++ NTMT/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     $(".load").hide();
     $(".loadone").delay(500).fadeIn("slow");
+    $(".loadtwo").delay(500).fadeIn("slow");
+    $("#sent1").delay(1000).fadeIn("slow");
+    $("#sent2").delay(1400).fadeIn("slow");
+    $("#sent3").delay(1600).fadeIn("slow");
+    $("#sent4").delay(1800).fadeIn("slow");
 
+    $("#sent5").delay(2000).fadeIn("slow");
+    $("#tag").delay(2300).fadeIn("slow");
 });
